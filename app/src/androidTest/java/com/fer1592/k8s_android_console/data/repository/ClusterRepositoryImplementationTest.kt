802143e4bf06aa3330acd845a81c3de5169f9189
Fernando Perez <fj.perezosan@gmail.com>
2022-05-14 19:05:58 -0300

    Added missing instrumented tests, run adv in ubuntu instead of mac

--- app/src/androidTest/java/com/fer1592/k8s_android_console/data/repository/ClusterRepositoryImplementationTest.kt
+++ app/src/androidTest/java/com/fer1592/k8s_android_console/data/repository/ClusterRepositoryImplementationTest.kt
@@ -55,12 +55,48 @@ class ClusterRepositoryImplementationTest {
 
     @ExperimentalCoroutinesApi
     @Test
+    fun addClusterEmptyName() = runTest {
+        cluster.clusterName = ""
+        val result = clusterRepository.addCluster(cluster)
+
+        Assert.assertTrue("Record was inserted into the database", !result)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun addClusterEmptyAddress() = runTest {
+        cluster.clusterAddress = ""
+        val result = clusterRepository.addCluster(cluster)
+
+        Assert.assertTrue("Record was inserted into the database", !result)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun addClusterPortOutOfRange() = runTest {
+        cluster.clusterPort = 0
+        val result = clusterRepository.addCluster(cluster)
+
+        Assert.assertTrue("Record was inserted into the database", !result)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun addClusterEmptyBearerToken() = runTest {
+        cluster.clusterBearerToken = ""
+        val result = clusterRepository.addCluster(cluster)
+
+        Assert.assertTrue("Record was inserted into the database", !result)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
     fun updateClusterWithIP() = runTest {
         clusterRepository.addCluster(cluster)
         cluster.clusterAddress = "192.168.10.20"
         val result = clusterRepository.updateCluster(cluster)
 
-        Assert.assertTrue("Record was not inserted into the database", result)
+        Assert.assertTrue("Record was not updated in the database", result)
     }
 
     @ExperimentalCoroutinesApi
@@ -70,6 +106,46 @@ class ClusterRepositoryImplementationTest {
         cluster.clusterAddress = "testAddress.com"
         val result = clusterRepository.updateCluster(cluster)
 
-        Assert.assertTrue("Record was not inserted into the database", result)
+        Assert.assertTrue("Record was not updated in the database", result)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun updateClusterEmptyName() = runTest {
+        clusterRepository.addCluster(cluster)
+        cluster.clusterName = ""
+        val result = clusterRepository.updateCluster(cluster)
+
+        Assert.assertTrue("Record was updated in the database", !result)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun updateClusterEmptyAddress() = runTest {
+        clusterRepository.addCluster(cluster)
+        cluster.clusterAddress = ""
+        val result = clusterRepository.updateCluster(cluster)
+
+        Assert.assertTrue("Record was updated in the database", !result)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun updateClusterPortOutOfRange() = runTest {
+        clusterRepository.addCluster(cluster)
+        cluster.clusterPort = 0
+        val result = clusterRepository.updateCluster(cluster)
+
+        Assert.assertTrue("Record was updated in the database", !result)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun updateClusterEmptyBearerToken() = runTest {
+        clusterRepository.addCluster(cluster)
+        cluster.clusterAddress = ""
+        val result = clusterRepository.updateCluster(cluster)
+
+        Assert.assertTrue("Record was updated in the database", !result)
     }
 }
